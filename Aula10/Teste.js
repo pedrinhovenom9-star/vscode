83733053f234b6fc1d14e4c4f490eb6c0bb299fc
Pedro Daniel <pedrinho.venom9@gmail.com>
2026-04-08 19:02:01 -0300

    Desafio final Senai Curso JS

---
+++ Aula10/Teste.js
@@ -0,0 +1,8 @@
+console.log("Olá mundo!");
+        alert("Bem vindo!");
+
+        let nome = prompt("Qual Seu nome ?");
+        console.log(`Seja bem vindo ${nome}`);
+
+        let ok = confirm("Deseja continuar?");
+        console.log(ok)
